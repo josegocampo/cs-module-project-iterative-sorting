1785b31e819c8f9e4d7ced7a69feae428453f2bc
josegocampo <jose.ocampo@mail.udp.cl>
2020-07-12 22:33:24 -0400

    done

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for num in range(0, len(arr)):
+        if arr[num] == target:
+            return num
 
     return -1   # not found
 
@@ -8,7 +10,18 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    for num in range(0, len(arr)):
+        if arr[num] == target:
+            return num
+        median = len(arr) / 2
+        arr_median_value = int(median)
+        if target < arr[arr_median_value]:
+            new_arr = arr[:arr_median_value]
+            binary_search(new_arr, target)
+        if target > arr[arr_median_value]:
+            new_arr = arr[arr_median_value:]
+            binary_search(new_arr, target)
+
 
 
     return -1  # not found
